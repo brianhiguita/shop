44c60599b54904473400124770d37494cb8135bf
unknown <brian.higuita@hotmail.co.uk>
2018-12-24 18:04:43 +0000

    sql added

--- classes/user.class.php
+++ classes/user.class.php
@@ -7,6 +7,7 @@ class User {
   public $password;
   public $user_exists;
 
+  // view user details
   public function get_user() {
     $email = $_SESSION['email'];
     $query = "SELECT * FROM `user` WHERE email = '$email'";
@@ -17,10 +18,9 @@ class User {
       $this->email = $row['email'] . "<br>";
       $this->password = $row['password'];
     }
-
   }
 
-
+  // create a user (entry in database)
   public function create_user() {
     global $database;
     if (isset($_POST['submit'])) {
@@ -38,62 +38,58 @@ class User {
         } else {
           echo "user already exists";
         }
-
       }
       else {
         echo "Not all fields were entered correctly";
       }
-
     }
-
   }
 
-
-
+  // login to user account
   public function login_user() {
-      if (isset($_POST['submit'])) {
-        if (!empty($_POST['email']) && !empty($_POST['password'])) {
+    if (isset($_POST['submit'])) {
+      if (!empty($_POST['email']) && !empty($_POST['password'])) {
 
-          global $database;
-          $email = mysqli_real_escape_string($database->connection, $_POST['email']);
-          $password = mysqli_real_escape_string($database->connection, $_POST['password']);
-          $password = md5($password);
-          $this->user_exists($email, $password);
+        global $database;
+        $email = mysqli_real_escape_string($database->connection, $_POST['email']);
+        $password = mysqli_real_escape_string($database->connection, $_POST['password']);
+        $password = md5($password);
+        $this->user_exists($email, $password);
 
-          if ($this->user_exists == true) {
-            $_SESSION['email'] = $email;
-            header('Location: index.php');
+        if ($this->user_exists == true) {
+          $_SESSION['email'] = $email;
+          header('Location: index.php');
 
-          } else {
-            echo "the user doesnt exists";
-          }
-
+        } else {
+          echo "the user doesnt exists";
         }
-        else {
-          echo "somethings empty";
-        }
+
       }
+      else {
+        echo "somethings empty";
+      }
+    }
   }
 
-
-
+  // does the user exists? login and register
   protected function user_exists($email, $password) {
-    $query = "SELECT * FROM `user` WHERE email = '$email' and password = '$password'";
-    $result = $this->user_query($query);
-      if (mysqli_num_rows($result)>=1) {
-        $this->user_exists = true;
-      }  else {
-        $this->user_exists = false;
-      }
+  $query = "SELECT * FROM `user` WHERE email = '$email' and password = '$password'";
+  $result = $this->user_query($query);
+    if (mysqli_num_rows($result)>=1) {
+      $this->user_exists = true;
+    }  else {
+      $this->user_exists = false;
+    }
   }
 
-
+  // sql queries for this class
   protected function user_query($sql) {
     global $database;
     $query = $database->query($sql);
     return $query;
   }
 
+  // checking if user is signed in
   public function is_signed_in() {
     if (!isset($_SESSION['email'])) {
       echo "user is not signed in, needs to be redirected";
@@ -101,7 +97,7 @@ class User {
     }
   }
 
-
+// end of class
 }
 
 
